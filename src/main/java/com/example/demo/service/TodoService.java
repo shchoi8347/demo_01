9b5b052fd4c20807512f7084599a1e95dfe75725
csh <csh@duksung.ac.kr>
2024-04-01 15:19:31 +0900

    Complete: 20240401(2장 백엔드 완성)

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,6 +26,49 @@ public class TodoService {
     }
 
     public List<TodoEntity> create(final TodoEntity entity) {
+        validate(entity);
+
+        repository.save(entity);
+
+        log.info("Entity Id: {} is saved", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
+    }
+
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
+
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+        original.ifPresent( todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            repository.save(todo);
+        }  );
+
+        return retrieve(entity.getUserId());
+
+    }
+
+    public List<TodoEntity> delete(final TodoEntity entity) {
+        validate(entity);
+
+        try {
+            repository.delete(entity);
+        } catch (Exception e) {
+            log.error("error deleting entity  ", entity.getId(), e);
+            throw new RuntimeException("error deleting entity " + entity.getId());
+        }
+
+        return retrieve(entity.getUserId());
+    }
+
+    public void validate(TodoEntity entity) {
         if( entity == null ) {
             log.warn("Entity cannot be null.");
             throw new RuntimeException("Entity cannot be null.");
@@ -34,13 +78,23 @@ public class TodoService {
             log.warn("Unknown user.");
             throw new RuntimeException("Unknown user.");
         }
-
-        repository.save(entity);
-
-        log.info("Entity Id: {} is saved", entity.getId());
-
-        return repository.findByUserId(entity.getUserId());
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
